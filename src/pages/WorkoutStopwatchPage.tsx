70f9f128374ce9160573ea9aef8bfe7243254b85
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2026-01-18 15:04:10 +0200

    `WorkoutStopwatchPage` add  `<Link to="">` to stop (exit from) a WorkoutStopwatch

--- src/pages/WorkoutStopwatchPage.tsx
+++ src/pages/WorkoutStopwatchPage.tsx
@@ -1,37 +1,40 @@
 import { useState } from "react"
+import { Link } from "react-router-dom";
 
 function WorkoutStopwatchPage() {
 
   const [isPaused, setPause] = useState(false)
 
   return (
-    <div className='workout-stopwatch-page'>
+      <div className='workout-stopwatch-page'>
 
-      <div className="row">
-        <button className='button-action' onClick={() => alert("todo")}> ⬅️ Prev </button>
-        <button className='button-action' onClick={() => alert("todo")}> ➡️ Next</button>
-      </div>
-
-      <div>
-        <h1>mm:ss</h1> {/* time left of the current timer */}
-        <h2>jumping jacks </h2>  {/* current timer name */}
-        <h3>set 1 of 4</h3> 
-        <h4>general fitness workout</h4> {/* current workout name */}
-      </div>
-
-      {!isPaused ? (
         <div className="row">
-          <button className='button-action' onClick={() => alert("todo")}> 🔁 Redo</button>
-          <button className='button-action' onClick={() => setPause(true)}> ⏸️ Pause</button>
+          <button className='button-action' onClick={() => alert("todo")}> ⬅️ Prev </button>
+          <button className='button-action' onClick={() => alert("todo")}> ➡️ Next</button>
         </div>
-      ) : (
-        <div className="row">
-          <button className='button-action' onClick={() => alert("todo")}> ⏹️ Stop (Exit)</button>
-          <button className='button-action' onClick={() => setPause(false)}> ▶️ Continue</button>
-        </div>
-      )}
 
-    </div>
+        <div>
+          <h1>mm:ss</h1> {/* time left of the current timer */}
+          <h2>jumping jacks </h2>  {/* current timer name */}
+          <h3>set 1 of 4</h3>
+          <h4>general fitness workout</h4> {/* current workout name */}
+        </div>
+
+        {!isPaused ? (
+          <div className="row">
+            <button className='button-action' onClick={() => alert("todo")}> 🔁 Redo</button>
+            <button className='button-action' onClick={() => setPause(true)}> ⏸️ Pause</button>
+          </div>
+        ) : (
+          <div className="row">
+            <Link to="/workouts">
+              <button className='button-action'> ⏹️ Stop (Exit)</button>
+            </Link>
+            <button className='button-action' onClick={() => setPause(false)}> ▶️ Continue</button>
+          </div>
+        )}
+
+      </div>
   )
 }
 
