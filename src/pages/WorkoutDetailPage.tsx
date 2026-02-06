62ded0c634fc3c90064630bfebbabf3bdcb0460f
maxstepanyuk linux <40894200+maxstepanyuk@users.noreply.github.com>
2026-02-06 14:41:09 +0200

    upd `TimerSection SetSection Workout` add props and pass Delete functions  (but without delete logic)

--- src/pages/WorkoutDetailPage.tsx
+++ src/pages/WorkoutDetailPage.tsx
@@ -3,7 +3,7 @@ import type { editMode } from "../models/util"
 import type { WorkoutDetailsPageProps, WorkoutProps, SetSectionProps, TimerSectionProps } from "../models/workout"
 import { Link } from "react-router-dom"
 
-function TimerSection({ minutes = 0, seconds = 0, name = 'timer', isEditMode = false }: TimerSectionProps & editMode) {
+function TimerSection({ id, minutes = 0, seconds = 0, name = 'timer', isEditMode = false, onTimerDelete }: TimerSectionProps & editMode) {
 
   const [nameValue, setNameValue] = useState(name)
   const [minutesValue, setMinutesValue] = useState(minutes)
@@ -38,7 +38,7 @@ function TimerSection({ minutes = 0, seconds = 0, name = 'timer', isEditMode = f
             onChange={(e) => handleUpdSecondsValue(Number(e.target.value))}
           />
         </div>
-        <button className="button-mini" onClick={() => alert("todo")}>delete timer</button>
+        <button className="button-mini" onClick={() => onTimerDelete(id)}>delete timer</button>
       </>) : (<>
         <p>{name}</p>
         <p >{minutes}:{seconds}
@@ -50,13 +50,24 @@ function TimerSection({ minutes = 0, seconds = 0, name = 'timer', isEditMode = f
   )
 }
 
-function SetSection({ repeat = 0, timers, isEditMode = false }: SetSectionProps & editMode) {
+function SetSection({ id, repeat = 0, timers, isEditMode = false, onSetDelete }: SetSectionProps & editMode) {
 
   const [timersData, setTimersData] = useState(timers)
 
+  function handleTimerDelete(index: number): void {
+    console.log('SetSection', id, '-> onTimerDelete', index)
+  }
+
   let timersHtml
   if (timersData && timersData.length > 0) {
-    timersHtml = timersData.map(timer => <TimerSection minutes={timer.minutes} seconds={timer.seconds} name={timer.name} isEditMode={isEditMode}></TimerSection>)
+    timersHtml = timersData.map((timer, index) =>
+      <TimerSection
+        key={index}
+        id={index}
+        minutes={timer.minutes} seconds={timer.seconds}
+        name={timer.name} isEditMode={isEditMode}
+        onTimerDelete={() => handleTimerDelete(index)}
+      />)
   } else {
     timersHtml = "no timers"
   }
@@ -83,7 +94,7 @@ function SetSection({ repeat = 0, timers, isEditMode = false }: SetSectionProps 
       {isEditMode && (
         <div className="row">
           <button className="button-mini" onClick={() => handleUpdRepeatValue(repeatValue - 1)}>remove set rep</button>
-          <button className="button-mini" onClick={() => alert("todo")}>delete set</button>
+          <button className="button-mini" onClick={() => onSetDelete(id)}>delete set</button>
           <button className="button-mini" onClick={() => handleUpdRepeatValue(repeatValue + 1)}>add set rep</button>
         </div>
       )}
@@ -99,9 +110,21 @@ function SetSection({ repeat = 0, timers, isEditMode = false }: SetSectionProps 
 
 function Workout({ name = "no name", description = "no description", sets, isEditMode = false }: WorkoutProps & editMode) {
 
+  function handleSetDelete(index: number): void {
+    console.log('Workout -> onSetDelete ', index)
+  }
+
   let setsHtml
   if (sets && sets.length > 0) {
-    setsHtml = sets.map(set => <SetSection repeat={set.repeat} timers={set.timers} isEditMode={isEditMode} />)
+    setsHtml = sets.map((set, index) =>
+      <SetSection
+        key={index}
+        id={index}
+        repeat={set.repeat} timers={set.timers}
+        isEditMode={isEditMode}
+        onSetDelete={() => handleSetDelete(index)}
+      />
+    )
   } else {
     setsHtml = "no sets"
   }
